9c00d9287aec9de9141e0be53405e76bd5e3e01b
vidimitrov <vidimi7rov@gmail.com>
2025-02-04 15:04:07 +0200

    feat: add PWA support

--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   reactStrictMode: true,
-  swcMinify: true,
   // Ensure TypeScript is used for all files
   typescript: {
     ignoreBuildErrors: false,
   },
   // Configure page extensions to only allow TypeScript
   pageExtensions: ["ts", "tsx"],
-};
+});
 
 module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,44 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Ale - Personal finance</title>
-        <link rel="icon" type="image/png" href="/logo.png" />
+        <meta name="application-name" content="ALE" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="ALE" />
+        <meta
+          name="description"
+          content="Track your income and expenses with ease"
+        />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          href="/icons/android/android-launchericon-192-192.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/ios/180.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icons/ios/152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/ios/180.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="167x167"
+          href="/icons/ios/167.png"
+        />
       </Head>
       <Component {...pageProps} />
     </>
